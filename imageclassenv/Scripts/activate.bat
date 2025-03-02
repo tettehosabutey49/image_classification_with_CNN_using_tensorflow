d2262f149d63bd109523f112d994a9f28711ebd1
Emmanuel Osabutey <tettehosabutey49@gmail.com>
2025-03-02 07:01:24 +0100

    Trained CNN

--- imageclassenv/Scripts/activate.bat
+++ imageclassenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Documentpc\Image Classification CNN using pytorch\imageclassenv
+set VIRTUAL_ENV=C:\Documentpc\Image Classification CNN using tensorflow\imageclassenv
 
 if not defined PROMPT set PROMPT=$P$G
 
